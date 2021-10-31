e0a40bf7d627dc93bd384ebb98f103b05858dc05
pmp38 <93110906+pmp38@users.noreply.github.com>
2021-10-31 16:54:29 +0700

    Update navigation.jsx

--- React-Landing-Page-Template-master/src/components/navigation.jsx
+++ React-Landing-Page-Template-master/src/components/navigation.jsx
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-          Asian Pacific Endowment for Science and Human
+          APESH
           </a>{' '}
         </div>
 
