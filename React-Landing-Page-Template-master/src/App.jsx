c2dd020d105f831e6fdc80f715e605ccd7dc469e
phamminhphu <phamminhphu@luvina.net>
2021-11-05 18:21:26 +0700

    update luv

--- React-Landing-Page-Template-master/src/App.jsx
+++ React-Landing-Page-Template-master/src/App.jsx
@@ -12,6 +12,23 @@ import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
 
+import SlideshowGallery from './components/slideshow-gallery/slideshow-gallery';
+import img1 from './images/01.jpg';
+import img2 from './images/02.jpg';
+import img3 from './images/03.jpg';
+import img4 from './images/04.jpg';
+import img5 from './images/05.jpg';
+import img6 from './images/06.jpg';
+
+const collection = [
+  { src: img1, caption: "Caption one" },
+  { src: img2, caption: "Caption two" },
+  { src: img3, caption: "Caption three" },
+  { src: img4, caption: "Caption four" },
+  { src: img5, caption: "Caption five" },
+  { src: img6, caption: "Caption six" },
+];
+
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
@@ -24,17 +41,26 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <><div>
       <Navigation />
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery}/>
+      <Gallery data={landingPageData.Gallery} />
       <Testimonials data={landingPageData.Testimonials} />
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
     </div>
+    <div>
+        <SlideshowGallery
+          input={collection}
+          ratio={`1920:1080`}
+          mode={`automatic`}
+          timeout={`3000`}
+        />
+    </div>
+    </>
   );
 };
 
